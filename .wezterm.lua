9c349644ea9a605782a0408561c877bcbe0006b9
Maksydenko <maksydenko@gmail.com>
2026-03-09 18:16:36 +0200

    feat: add wezterm config

---
+++ .wezterm.lua
@@ -0,0 +1,20 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices.
+
+-- For example, changing the initial geometry for new windows:
+config.initial_cols = 120
+config.initial_rows = 28
+
+-- or, changing the font size and color scheme.
+config.font = wezterm.font 'JetBrainsMono NF'
+config.font_size = 14
+config.color_scheme = 'Dracula'
+config.window_background_opacity = 0.9
+
+-- Finally, return the configuration to wezterm:
+return config
